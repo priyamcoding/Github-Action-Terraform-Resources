7eb764db9d94856737ec750983e25559e44ff9fe
priyamcoding <priyammahajan17@gmail.com>
2022-09-30 17:05:02 +0530

    hanged file names

--- modules/main.tf
+++ modules/main.tf
@@ -1,11 +1,11 @@
 module "s3-mod" {
   source = "../s3"
-  bucketName = "priyam-bucket1"
+  bucketName = "priyam-bucket2"
   acl-value =  "private"
 }
 
 module "ec2-mod" {
   source = "../ec2"
   instance_type =  "t2.micro"
-  instanceName = "priyam-instance1"
+  instanceName = "priyam-instance2"
 }
